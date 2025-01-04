c9730ca46a70c3f4e2f4dc8c61ae773bafcc911a
Aum Soni <96745193+aumsoni2002@users.noreply.github.com>
2025-01-04 13:38:24 -0500

    feat: ShareButton and ImageContainer Component complete

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,8 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import PropertyRating from "@/components/card/PropertyRating";
 import Breadcrumbs from "@/components/properties/BreadCrumbs";
+import ImageContainer from "@/components/properties/ImageContainer";
+import ShareButton from "@/components/properties/ShareButton";
 import { fetchPropertyDetails } from "@/utils/actions";
 import { redirect } from "next/navigation";
 
@@ -17,10 +20,23 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
       <header className="flex justify-between items-center mt-4">
         <h1 className="text-4xl font-bold capitalize">{property.tagline}</h1>
         <div className="flex items-center gap-x-4">
-          {/* share button */}
+          <ShareButton name={property.name} propertyId={property.id} />
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          {/* calendar */}
+        </div>
+      </section>
     </section>
   );
 }
